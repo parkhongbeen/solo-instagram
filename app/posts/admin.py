73d5686eeb2530fc56e203810cac8af90f632ec1
parkhongbeen <pack122@naver.com>
2020-01-14 13:27:05 +0900

    media_url 설정

--- app/posts/admin.py
+++ app/posts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from posts.models import Post, PostImage, PostLike, PostComment
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PostImage)
+class PostImageAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PostComment)
+class PostCommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PostLike)
+class PostLikeAdmin(admin.ModelAdmin):
+    pass
--- app/posts/models.py
+++ app/posts/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from members.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(blank=True)
+    like_user = models.ManyToManyField(
+        User, through='PostLike', related_name='like_post_set'
+    )
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class PostImage(models.Model):
+    post = models.ForeignKey(Post, on_delete=True)
+    image = models.ImageField(upload_to='posts/images')
+
+
+class PostComment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+
+
+class PostLike(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
